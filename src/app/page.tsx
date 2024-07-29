cf14d1251a8bb7ab853ccf485413aa5f3d972fd1
xulihang <xulihanghai@163.com>
2024-07-29 15:59:52 +0800

    add MRZScanner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+"use client"
 import styles from "./page.module.css";
+import { useState } from "react";
+import MRZScanner from "@/components/MRZScanner";
 
 export default function Home() {
+  const [isScanning,setIsScanning] = useState(false);
   return (
     <main className={styles.main}>
       <h2>MRZ Scanner</h2>
-      <button>Start Scanning</button>
+      <button onClick={()=>setIsScanning(!isScanning)} >{isScanning?"Stop Scanning":"Start Scanning"}</button>
+      <div className={styles.cameraContainer}>
+        <MRZScanner isScanning={isScanning}></MRZScanner>
+      </div>
     </main>
   );
 }
