d655f4b3481a684a30b04fa9842566297a06db03
xulihang <xulihanghai@163.com>
2024-07-29 17:44:51 +0800

    add MRZ result table component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,25 @@
 import styles from "./page.module.css";
 import { useState } from "react";
 import MRZScanner from "@/components/MRZScanner";
+import { RecognizedTextLinesResult } from "dynamsoft-label-recognizer-bundle";
+import MRZResultTable from "@/components/MRZResultTable";
 
 export default function Home() {
   const [isScanning,setIsScanning] = useState(false);
   const [initialized,setInitialized] = useState(false);
+  const [MRZ,setMRZ] = useState("");
+
+  const onScanned = (result:RecognizedTextLinesResult) => {
+    setIsScanning(false);
+    console.log(result);
+    if (result.textLineResultItems.length>0) {
+      let str = "";
+      str = result.textLineResultItems[0].text
+      console.log("combined:"+str)
+      setMRZ(str);
+    }
+  }
+
   return (
     <main className={styles.main}>
       <h2>MRZ Scanner</h2>
@@ -15,15 +30,16 @@ export default function Home() {
       {initialized &&(
         <button onClick={()=>setIsScanning(!isScanning)} >{isScanning?"Stop Scanning":"Start Scanning"}</button>
       )}
-      <div className={styles.scanner}>
-        <div className={styles.cameracontainer + ((initialized && isScanning) ? "" : " "+styles.hidden)}>
+      <div className={styles.scanner + ((initialized && isScanning) ? "" : " "+styles.hidden)}>
+        <div className={styles.cameracontainer}>
           <MRZScanner 
             isScanning={isScanning}
-            initialized={()=>{setInitialized(true)}}
+            onScanned={(result:RecognizedTextLinesResult)=>{onScanned(result)}}
+            onInitialized={()=>{setInitialized(true)}}
           ></MRZScanner>
         </div>
       </div>
-      
+      <MRZResultTable MRZ={MRZ}></MRZResultTable>
     </main>
   );
 }
